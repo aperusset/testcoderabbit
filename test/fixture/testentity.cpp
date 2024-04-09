4281bcdfbcf3896ee3049d144877903635bd73d3
Alexandre Perusset <aperusset@gmail.com>
2024-04-09 20:31:49 +0200

    Remove useless files

--- test/fixture/testentity.cpp
+++
@@ -1,7 +0,0 @@
-#include "testentity.h"
-
-TestEntity::TestEntity(const int key) : Entity(key) {}
-
-auto aTestEntity(int key) -> std::unique_ptr<TestEntity> {
-    return std::unique_ptr<TestEntity>( new TestEntity(key) );
-}
