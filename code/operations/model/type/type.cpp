79ecd335134b9667de1544776c7444ba1b16587b
Alexandre Perusset <aperusset@gmail.com>
2024-04-09 20:39:26 +0200

    remove some files

--- code/operations/model/type/type.cpp
+++
@@ -1,8 +0,0 @@
-#include "operations/model/type/type.h"
-#include <utility>
-
-QORM::Type::Type(QString name) : name(std::move(name)) {}
-
-auto QORM::Type::generate() const -> QString {
-    return this->name;
-}
--- code/operations/model/type/varchar.cpp
+++
@@ -1,6 +0,0 @@
-#include "operations/model/type/varchar.h"
-
-const QString QORM::Varchar::EMPTY = "''";
-
-QORM::Varchar::Varchar(const int size) :
-    Type("varchar(" + QString::number(size) + ")") {}
