54ea6c13b523e516d796ce00b94e1bcda443ded2
Alexandre Perusset <aperusset@gmail.com>
2024-04-17 14:32:45 +0200

    Coderabbit main

---
+++ database.cpp
@@ -0,0 +1,101 @@
+#include "database.h"
+#include <algorithm>
+
+QORM::Database::Database(const QORM::Connector &connector, bool verbose) :
+    databaseMutex(QMutex::RecursionMode::Recursive),
+    connector(connector), creator(nullptr), verbose(verbose) {
+}
+
+
+QORM::Database::Database(const QORM::Connector &connector,
+                         const QORM::Creator &creator, bool verbose) :
+    databaseMutex(QMutex::RecursionMode::Recursive),
+    connector(connector), creator(&creator), verbose(verbose) {
+}
+
+QORM::Database::~Database() {
+    this->disconnect();
+}
+
+auto QORM::Database::prepare(const QString &query) const -> QSqlQuery {
+    QSqlQuery sqlQuery(connector.getDatabase());
+    if (!sqlQuery.prepare(query + ";")) {
+        throw std::string("Preparing error : ") +
+                sqlQuery.lastError().text().toStdString() +
+                " | Query : " +
+                query.toStdString();
+    }
+    return sqlQuery;
+}
+
+auto QORM::Database::prepare(const Query &query) const -> QSqlQuery {
+    return query.bind(this->prepare(query.generate()));
+}
+
+auto QORM::Database::execute(const QString &query) const -> QSqlQuery {
+    return this->execute(this->prepare(query));
+}
+
+auto QORM::Database::execute(const Query &query) const -> QSqlQuery {
+    return this->execute(this->prepare(query));
+}
+
+auto QORM::Database::execute(QSqlQuery query) const -> QSqlQuery {
+    if (this->verbose) {
+        qDebug("%s", qUtf8Printable(query.lastQuery()));
+    }
+    auto const success = query.exec();
+    if (!success && query.lastError().isValid()) {
+        throw std::string("Query error : ") +
+                query.lastQuery().toStdString() + " (" +
+                query.lastError().driverText().toStdString() + ")";
+    }
+    return query;
+}
+
+auto QORM::Database::getName() const -> QString {
+    return this->connector.getName();
+}
+
+auto QORM::Database::isConnected() const -> bool {
+    return connector.isConnected();
+}
+
+auto QORM::Database::connect() -> bool {
+    const QMutexLocker lock(&databaseMutex);
+    if (!this->isConnected()) {
+        this->connector.connect();
+        if (this->creator != nullptr) {
+            auto const shouldBeCreated = !this->creator->isCreated(*this,
+                this->connector.tables(), this->connector.views());
+            if (shouldBeCreated) {
+                qDebug(
+                    "Create database with name %s",
+                    qUtf8Printable(connector.getName()));
+                this->creator->createAllAndPopulate(*this);
+            }
+            return shouldBeCreated;
+        }
+    }
+    return false;
+}
+
+void QORM::Database::disconnect() {
+    const QMutexLocker lock(&databaseMutex);
+    connector.disconnect();
+}
+
+void QORM::Database::optimize() const {
+    this->connector.optimize();
+}
+
+auto QORM::Database::backup(const QString &fileName) -> bool {
+    const QMutexLocker lock(&databaseMutex);
+    return connector.backup(fileName);
+}
+
+auto QORM::Database::exists(const QString &table,
+                            const std::list<Condition> &conditions)
+const -> bool {
+    return this->execute(Select(table).where(conditions)).next();
+}
---
+++ utils.cpp
@@ -0,0 +1,140 @@
+#include "utils.h"
+#include <QRandomGenerator>
+#include <QRegExp>
+#include "operations/query/selection/dateformatter.h"
+
+namespace {
+
+auto formatSQLDate(const QString &format, const QString &fieldName,
+                   const QString &renameTo) -> QORM::Selection {
+    return QORM::DateFormatter(format, fieldName, renameTo);
+}
+
+}  // namespace
+
+auto QORM::Utils::formatSQLiteDate(const QDate &date) -> QString {
+    return date.toString("yyyy-MM-dd");
+}
+
+auto QORM::Utils::backupFileName(const QString& databaseName) -> QString {
+    return "backup_" + databaseName + "_" +
+            formatSQLiteDate(QDate::currentDate()) + ".db";
+}
+
+auto QORM::Utils::parametrize(const QString &field) -> QString {
+    return ":" + field.toLower().remove(QRegExp("[.() ]"));
+}
+
+auto QORM::Utils::dateToDay(const QString &fieldName,
+                          const QString &renameTo) -> Selection {
+    return formatSQLDate("%d", fieldName, renameTo);
+}
+
+auto QORM::Utils::dateToMonth(const QString &fieldName,
+                            const QString &renameTo) -> Selection {
+    return formatSQLDate("%m", fieldName, renameTo);
+}
+
+auto QORM::Utils::dateToYear(const QString &fieldName,
+                           const QString &renameTo) -> Selection {
+    return formatSQLDate("%Y", fieldName, renameTo);
+}
+
+auto QORM::Utils::null() -> QVariant {
+    return QVariant();
+}
+
+auto QORM::Utils::qualifyFieldName(const QString &qualifier,
+                                   const QString &fieldName) -> QString {
+    return qualifier + "." + fieldName;
+}
+
+auto QORM::Utils::getBoolOrDefault(const QSqlRecord &record,
+                                   const QString &fieldName,
+                                   bool defaultValue) -> bool {
+    return getOrDefault<bool>(record, fieldName, defaultValue,
+                              &QVariant::toBool);
+}
+
+auto QORM::Utils::getStringOrDefault(const QSqlRecord &record,
+                                     const QString &fieldName,
+                                     const QString &defaultValue) -> QString {
+    return getOrDefault<QString>(record, fieldName, defaultValue,
+                                 &QVariant::toString);
+}
+
+auto QORM::Utils::getDateOrDefault(const QSqlRecord &record,
+                                   const QString &fieldName,
+                                   const QDate &defaultValue) -> QDate {
+    return getOrDefault<QDate>(record, fieldName, defaultValue,
+                               &QVariant::toDate);
+}
+
+auto QORM::Utils::getDateTimeOrDefault(const QSqlRecord &record,
+                                       const QString &fieldName,
+                                       const QDateTime &defaultValue)
+    -> QDateTime {
+    return getOrDefault<QDateTime>(record, fieldName, defaultValue,
+                                   &QVariant::toDateTime);
+}
+
+auto QORM::Utils::getUIntOrDefault(const QSqlRecord &record,
+                                   const QString &fieldName,
+                                   uint32_t defaultValue) -> uint32_t {
+    return getOrDefault<uint32_t>(record, fieldName, defaultValue,
+                                  [](const QVariant &variant) -> uint32_t {
+                                    return variant.toUInt();
+                                  });
+}
+
+auto QORM::Utils::getIntOrDefault(const QSqlRecord &record,
+                                  const QString &fieldName,
+                                  int32_t defaultValue) -> int32_t {
+    return getOrDefault<int32_t>(record, fieldName, defaultValue,
+                                 [](const QVariant &variant) -> int32_t {
+                                   return variant.toInt();
+                                 });
+}
+
+auto QORM::Utils::getDoubleOrDefault(const QSqlRecord &record,
+                                     const QString &fieldName,
+                                     double defaultValue) -> double {
+    return getOrDefault<double>(record, fieldName, defaultValue,
+                                [](const QVariant &variant) -> double {
+                                  return variant.toDouble();
+                                });
+}
+
+auto const validString = [](const QString &value) {
+    return !value.isEmpty();
+};
+
+auto QORM::Utils::notBlankOrNull(const QString &value) -> QVariant {
+    return validOrNull<QString>(value.trimmed(), validString);
+}
+
+auto QORM::Utils::notBlankOrThrow(const QString &value) -> QVariant {
+    return validOrThrow<QString>(value.trimmed(),
+                                 "A not blank string is expected", validString);
+}
+
+auto const validDate = [](const QDate &value) {
+    return value.isValid();
+};
+
+auto QORM::Utils::validOrNull(const QDate &value) -> QVariant {
+    return validOrNull<QDate>(value, validDate);
+}
+
+auto QORM::Utils::validOrThrow(const QDate &value) -> QVariant {
+    return validOrThrow<QDate>(value, "A valid date is expected", validDate);
+}
+
+auto QORM::Utils::validOrNull(const QDateTime &value) -> QVariant {
+    return validOrNull<QDateTime>(value, &QDateTime::isValid);
+}
+
+auto QORM::Utils::validOrThrow(const QDateTime &value) -> QVariant {
+    return validOrThrow<QDateTime>(value, "A valid date time is expected",
+                                   &QDateTime::isValid);
+}
